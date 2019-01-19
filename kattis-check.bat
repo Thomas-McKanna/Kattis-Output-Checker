57378a80a7d73fc3a3ed736abb7460299447513d
Thomas McKanna <thomasmckanna@gmail.com>
2019-01-19 17:40:33 -0600

    Checks sample files with program output using diff.

--- kattis-check.bat
+++ kattis-check.bat
@@ -1,15 +1,43 @@
+@ECHO OFF
+setlocal enabledelayedexpansion
 SET tag=%1
+SET program=%2
 SET url=https://open.kattis.com/problems/%tag%/file/statement/samples.zip
+SET on=1
+SET off=0
 
 IF NOT EXIST samples (
-    mkdir samples
-    cd samples
+    MKDIR samples
+    CD samples
     curl %url% --output temp.zip
     7z x temp.zip -y -r
     rm temp.zip
-    cd ..
+    CD ..
 ) ELSE (
-    echo samples already existed
+    @ECHO ON
+    ECHO samples already existed
+    @ECHO OFF
 )
 
-echo *.exe
+SET flag=%on%
+FOR %%I IN (samples\*) DO (
+    IF "!flag!"=="%on%" (
+        SET ans_file=%%I
+        SET flag=%off%
+    ) ELSE (
+        %program% < %%I > temp.txt
+        diff -y temp.txt !ans_file!
+        IF "!errorlevel!"=="1" (
+            @ECHO ON
+            ECHO Failed: output with %%I did not match !ans_file!
+            @ECHO OFF
+        ) ELSE (
+            @ECHO ON
+            ECHO Success: output with %%I matched !ans_file!
+            @ECHO OFF
+        )
+        SET flag=%on%
+    )
+)
+rm temp.txt
+@ECHO ON
